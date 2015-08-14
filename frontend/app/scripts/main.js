20c6b11606261b9e8c23581e4bdab9b9275edd53
frescogamba <gordo@yugo.at>
2015-08-14 13:32:55 +0200

    added videoplayer, fixed fullscreen

--- frontend/app/scripts/main.js
+++ frontend/app/scripts/main.js
@@ -67,6 +67,12 @@
 			});
 		},
 
+		utils: {
+			ellipseString: function(_text, _length) {
+				return _text.substr(0, _length - 3) + "...";
+			},
+		},
+
 		/**
 		 * init the menu for the list of playlists
 		 */
@@ -197,24 +203,67 @@
 		 * start playback of current selected video
 		 */
 		startPlayback: function() {
-			var ID = YTP.worksIndex[YTP.currentArtist];
-			var videoID = YTP.videosIndex[YTP.currentVideo];
+
 			YTP.state = 2;
 			console.log('... playback');
-			console.log(YTP.works[ID]['Videos']['Video'][videoID]);
+			$('#bgModal').fadeIn().promise().done(function() {
+				YTP.initPlayer();
+			})
+
+		},
+
+		initPlayer: function() {
+			var ID = YTP.worksIndex[YTP.currentArtist];
+			var videoID = YTP.videosIndex[YTP.currentVideo];
+			YTP.updateSubMenu();
 			$('#jquery_jplayer_1').jPlayer('destroy').jPlayer({
 				ready: function() {
 					$(this).jPlayer('setMedia', {
-						m4v: YTP.works[ID]['Videos'] + '/' + YTP.works[ID]['Videos']['Video'][videoID]['filename'],
+						m4v: 'assets/' + YTP.works[ID]['PlaylistID'] + '/' + YTP.works[ID]['Videos']['Video'][videoID]['filename'],
 					});
+					$(this).jPlayer('play');
+					$('#jquery_jplayer_1').fadeIn('100').promise().done();
+					$('#jquery_jplayer_1').css('width', '100%');
+					$('#jquery_jplayer_1').css('height', '100%');
+					$('#jquery_jplayer_1 > video').css('width', '100%');
+					$('#jquery_jplayer_1 > video').css('height', '100%');
 				},
 				swfPath: './scripts/',
 				supplied: 'm4v',
 				solution: "html",
+				// size: {
+				// 	width: YTP.works[ID]['Videos']['Video'][videoID]['resolution'][0],
+				// 	height: YTP.works[ID]['Videos']['Video'][videoID]['resolution'][1]
+				// },
 				volume: 0.8,
 				muted: false,
 				keyEnabled: true,
+				fullScreen: true,
 				cssSelectorAncestor: '#jp_container_1',
+				cssSelector: {
+					videoPlay: '.jp-video-play',
+					play: '.jp-play',
+					pause: '.jp-pause',
+					stop: '.jp-stop',
+					seekBar: '.jp-seek-bar',
+					playBar: '.jp-play-bar',
+					mute: '.jp-mute',
+					unmute: '.jp-unmute',
+					volumeBar: '.jp-volume-bar',
+					volumeBarValue: '.jp-volume-bar-value',
+					volumeMax: '.jp-volume-max',
+					playbackRateBar: '.jp-playback-rate-bar',
+					playbackRateBarValue: '.jp-playback-rate-bar-value',
+					currentTime: '.jp-current-time',
+					duration: '.jp-duration',
+					title: '.jp-title',
+					fullScreen: '.jp-full-screen',
+					restoreScreen: '.jp-restore-screen',
+					repeat: '.jp-repeat',
+					repeatOff: '.jp-repeat-off',
+					gui: '.jp-gui',
+					noSolution: '.jp-no-solution'
+				},
 				keyBindings: {
 					play: {
 						key: 32, // p
@@ -226,8 +275,41 @@
 							}
 						}
 					}
+				},
+				ended: function() {
+
+					YTP.reinitPlayer(true);
+
+				},
+				resize: function() {
+
 				}
 			});
+		},
+
+		reinitPlayer: function(_direction) {
+			$('#jquery_jplayer_1').fadeOut('150').promise().done(function() {
+				$('#jquery_jplayer_1').jPlayer('destroy');
+
+				if (_direction === true) {
+					YTP.currentVideo += 1;
+					if (YTP.currentVideo >= YTP.totalVideos) {
+						YTP.currentVideo = 0;
+						YTP.stopPlayback();
+					} else {
+						YTP.initPlayer();
+					}
+				} else {
+					YTP.currentVideo -= 1;
+					if (YTP.currentVideo < 0) {
+						YTP.stopPlayback();
+					} else {
+						YTP.initPlayer();
+					}
+				}
+
+			});
+
 		},
 
 		/**
@@ -235,6 +317,26 @@
 		 */
 		stopPlayback: function() {
 			console.log('stop playback ...');
+
+			$('#jquery_jplayer_1').jPlayer('destroy');
+			$('#bgModal').fadeOut().promise().done(function() {
+				YTP.state = 1;
+				YTP.showHideMenu();
+			})
+
+		},
+
+		/**
+		 * Show help modal
+		 */
+		toggleHelp: function() {
+			if ($('#helpModal').isVisible()) {
+				$('#helpModal').fadeOut();
+
+			} else {
+				$('#helpModal').fadeIn();
+			}
+
 		},
 
 		/**
@@ -244,10 +346,9 @@
 		nextVideo: function(_direction) {
 			if (_direction === true) {
 				console.log('forward next video');
-				YTP.updateCounterDetail(_direction);
+				YTP.reinitPlayer(_direction);
 			} else {
-				console.log('backward video');
-				YTP.updateCounterDetail(_direction);
+				YTP.reinitPlayer(_direction);
 			}
 		},
 
@@ -256,6 +357,12 @@
 		 */
 		bindKeyHandlers: function() {
 			$(document).keydown(function(e) {
+				switch (e.which) {
+					case 27: // ESC
+						YTP.toggleHelp();
+						break;
+				}
+				//console.log(e.which);
 				switch (YTP.state) {
 					case 0:
 						switch (e.which) {
@@ -302,7 +409,6 @@
 								YTP.state = 1;
 								YTP.stopPlayback();
 								YTP.showHideMenu();
-								console.log('exit playback go back to menu');
 								break;
 							case 38: // up
 								YTP.nextVideo(false);
